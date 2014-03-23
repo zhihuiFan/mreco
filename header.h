90fc7d490d19a6f1e583228fcbf30376166ca4cd
Fan zhihui <zhifan@ebay.com>
2014-03-22 21:33:54 -0700

    add common used header files to header.h

---
+++ header.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <cassert>
+#include <cstdlib>
+
+using namespace std;
