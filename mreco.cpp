f060af3db92c2fe1d3fe86c8cb73b17297626fb1
Fan zhihui <zhifan@ebay.com>
2014-03-24 19:34:27 -0700

    added mreco.cpp, the main fuction is in it

---
+++ mreco.cpp
@@ -0,0 +1,69 @@
+#include "pdfile.h"
+#include <mongo/client/dbclient.h>
+#include <boost/program_options.hpp>
+#include <unistd.h>
+
+namespace po = boost::program_options;
+
+int main(int argc, char** argv) {
+  string dbpath, dbname, target, collection;
+  // bool deleted = false;
+
+  po::options_description desc("Allowed options");
+  desc.add_options()("help,h", "show this message and exit")(
+      "dbpath,p", po::value<string>(&dbpath),
+      "MUST, datafile directory, if directoryperdb, specify one directory "
+      "once")("db", po::value<string>(&dbname),
+              "MUST, database name we need to recover data from")(
+      "target,t", po::value<string>(&target),
+      "MUST, a temporary mongo host with default 27017 port to store the "
+      "recovered data")(
+      "coll,c", po::value<string>(&collection),
+      "MUST, format: db.coll. a collection to store recovered data");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help") || !vm.count("dbpath") || !vm.count("db") ||
+      !vm.count("target") || !vm.count("coll")) {
+    cout << desc << endl;
+    return 0;
+  }
+
+  if (count(collection.begin(), collection.end(), '.') != 1) {
+    cerr << "format -c db.collection" << endl;
+    return -1;
+  }
+
+  mongo::DBClientConnection tconn;
+  try{
+      tconn.connect(target.c_str());
+  } catch (mongo::DBException &e){
+      cout << "connect to target ERROR: " << e.what() << endl;
+      return -1;
+  }
+
+  if (dbpath[dbpath.size()-1] != '/')
+      dbpath.push_back('/');
+      
+
+  Database db(dbpath, dbname);
+  string fl = dbname + ".$freelist";
+  Collection * freelist = db.getns(fl);
+
+  DiskLoc cur = freelist->firstExt;
+  do {
+    list<mongo::BSONObj> data;
+    Extent *e = db.builtExt(cur);
+    e->dumpRows(data);
+    list<mongo::BSONObj>::iterator end = data.end();
+    for(list<mongo::BSONObj>::iterator it = data.begin(); it != end; it++){
+        tconn.insert(collection.c_str(), *it);
+    }
+    cout << "Recovered " << data.size() << " rows in this extent " << endl;
+    cur = e->xnext;
+  }while(cur != freelist->lastExt);
+
+  return 0;
+}
