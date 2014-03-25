a2ebbf3ae2f4398d10370076f954a2274d33405c
Fan zhihui <zhifan@ebay.com>
2014-03-24 19:46:49 -0700

    added more help message and format the code again

--- mreco.cpp
+++ mreco.cpp
@@ -5,7 +5,7 @@
 
 namespace po = boost::program_options;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   string dbpath, dbname, target, collection;
   // bool deleted = false;
 
@@ -27,7 +27,17 @@ int main(int argc, char** argv) {
 
   if (vm.count("help") || !vm.count("dbpath") || !vm.count("db") ||
       !vm.count("target") || !vm.count("coll")) {
-    cout << desc << endl;
+    cout << endl << desc << endl;
+    cout << "You can safely ingore the message like this:" << endl;
+    cout << "assertion failure in bson library: 10334 Invalid BSONObj size: "
+            "1666293760 (0x00A05163) first element: : "
+            "ObjectId('00feffffff00000001009400')" << endl;
+    cout << "they are not normal data. maybe index record?  Since these "
+            "message are printed from BSON libary, so I can't avoid them"
+         << endl;
+    cout << endl << "Note:  the program will recover all the data "
+                    "which was dropped in the given database,  it have "
+                    "nothing with the --coll option" << endl;
     return 0;
   }
 
@@ -37,20 +47,19 @@ int main(int argc, char** argv) {
   }
 
   mongo::DBClientConnection tconn;
-  try{
-      tconn.connect(target.c_str());
-  } catch (mongo::DBException &e){
-      cout << "connect to target ERROR: " << e.what() << endl;
-      return -1;
+  try {
+    tconn.connect(target.c_str());
+  }
+  catch (mongo::DBException &e) {
+    cout << "connect to target ERROR: " << e.what() << endl;
+    return -1;
   }
 
-  if (dbpath[dbpath.size()-1] != '/')
-      dbpath.push_back('/');
-      
+  if (dbpath[dbpath.size() - 1] != '/') dbpath.push_back('/');
 
   Database db(dbpath, dbname);
   string fl = dbname + ".$freelist";
-  Collection * freelist = db.getns(fl);
+  Collection *freelist = db.getns(fl);
 
   DiskLoc cur = freelist->firstExt;
   do {
@@ -58,12 +67,12 @@ int main(int argc, char** argv) {
     Extent *e = db.builtExt(cur);
     e->dumpRows(data);
     list<mongo::BSONObj>::iterator end = data.end();
-    for(list<mongo::BSONObj>::iterator it = data.begin(); it != end; it++){
-        tconn.insert(collection.c_str(), *it);
+    for (list<mongo::BSONObj>::iterator it = data.begin(); it != end; it++) {
+      tconn.insert(collection.c_str(), *it);
     }
     cout << "Recovered " << data.size() << " rows in this extent " << endl;
     cur = e->xnext;
-  }while(cur != freelist->lastExt);
+  } while (cur != freelist->lastExt);
 
   return 0;
 }
